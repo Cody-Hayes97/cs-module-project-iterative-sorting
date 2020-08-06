232e7b86359642abb2a5e7ec9f1382f1cd3bbc60
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-08-05 18:04:18 -0700

    initial pull

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -22,6 +22,29 @@ def bubble_sort(arr):
 
     return arr
 
+
+nums = [2,6,8,3,4,1,9,5,7]
+def insert_sort(nums):
+    # 1st element is a sorted list of '1'
+    # for n in nums
+        # while n > left hand side OR current index of n is not 0
+            # swap
+    for i in range(1, len(nums)):
+        temp = nums[i]
+        j = i 
+        # while were not at the beginning AND the value is not in the correct position yet
+        while j > 0 and temp < nums[j - 1]:
+            nums[j] = nums[j-1]
+            j -= 1
+        nums[j] = temp 
+
+print(nums)
+insert_sort(nums)
+print(nums)
+
+
+
+
 '''
 STRETCH: implement the Counting Sort function below
 
