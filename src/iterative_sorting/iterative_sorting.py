2787dda7d9ff88ca92a5e6939d73298dcf591bf7
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-08-05 20:28:25 -0700

    finished mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,28 +2,40 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
+       
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]: 
+                smallest_index = j
+        if smallest_index != i:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        
 
         # TO-DO: swap
         # Your code here
 
     return arr
 
+nums = [2,6,8,3,4,1,9,5,7]
+selection_sort(nums)
+print(nums)
+
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    sorted_i = len(arr) - 1
+    while sorted_i > 0:
+        for i in range(0, sorted_i):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        sorted_i -= 1
 
     return arr
 
 
-nums = [2,6,8,3,4,1,9,5,7]
 def insert_sort(nums):
     # 1st element is a sorted list of '1'
     # for n in nums
@@ -38,9 +50,7 @@ def insert_sort(nums):
             j -= 1
         nums[j] = temp 
 
-print(nums)
-insert_sort(nums)
-print(nums)
+
 
 
 
